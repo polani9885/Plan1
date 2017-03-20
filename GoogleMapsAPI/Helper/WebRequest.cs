fb90bb9ea83090abbdb884eb9b7ac92c110cf83f
polani9885 <srinivasinusa@gmail.com>
2017-03-19 22:04:56 -0400

    s

---
+++ GoogleMapsAPI/Helper/WebRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsAPI.Helper
+{
+    public class WebRequest
+    {
+        public string WebServiceInformation(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Method = "GET";
+                String test = String.Empty;
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    test = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                }
+                return test;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ GoogleMapsAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using GoogleMapsAPI.Helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsAPI
+{
+    class Program
+    {
+        private readonly WebRequest webRequest = new WebRequest();
+        
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.GetRadiusInformation();
+        }
+
+        public void GetRadiusInformation()
+        {
+            string radiusData = string.Empty;
+            string url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=27.987276,-82.334912&radius=500&key=" + ConfigurationManager.AppSettings["apiKey"];
+            radiusData = webRequest.WebServiceInformation(url);
+        }
+    }
+}
