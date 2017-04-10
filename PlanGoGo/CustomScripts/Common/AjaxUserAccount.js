fe1958e8b4d2bffdabeb505c7d5eb3d545879456
Srinivas.Polani <Srinivas.Polani@pharmerica.com>
2017-04-10 15:46:57 -0400

    a

--- PlanGoGo/CustomScripts/Common/AjaxUserAccount.js
+++ PlanGoGo/CustomScripts/Common/AjaxUserAccount.js
@@ -1,6 +1,6 @@
 ﻿function CheckUserNameExisted(username) {
     debugger;
-    if (username!=null && username != "") {
+    if (username!=null && username!="") {
         $.ajax({
             type: "GET",
             url: '/UserControls/UserExistChecking',
@@ -20,4 +20,125 @@
             }
         });
     }
+}
+
+function LoginCheck() {
+    debugger;
+    if ($("#txtLoginUsername").val() != null && $("#txtLoginUsername").val() != "") {
+        $.ajax({
+            type: "GET",
+            url: '/UserControls/User_GetUserInformation',
+            dataType: "json",
+            data: { userName: $("#txtLoginUsername").val(), password: $("#txtLoginPassword").val() },
+            beforeSend: function () {
+                $(".blockPage").show();
+            },
+            success: function (data) {
+                if (data.length > 0) {
+                    $("#lblLoginUsernameCheckError").hide();
+                    IsUserLoggedIn();
+                } else {
+                    $("#lblLoginUsernameCheckError").show();
+                }
+            },
+            error: function (result) {
+                alert('Service call failed: ' + result.status + ' Type :' + result.statusText);
+            },
+            complete: function () {
+                $(".blockPage").hide();
+            }
+        });
+    }
+}
+
+function IsUserLoggedIn() {
+    debugger;
+    $.ajax({
+        type: "GET",
+        url: '/UserControls/IsUserLoggedIn',
+        dataType: "json",
+        beforeSend: function() {
+            $(".blockPage").show();
+        },
+        success: function(data) {
+            if (data.success) {
+                userLoggedIn();
+            } else {
+                userLoggOut();
+            }
+        },
+        error: function(result) {
+            alert('Service call failed: ' + result.status + ' Type :' + result.statusText);
+        },
+        complete: function() {
+            $(".blockPage").hide();
+        }
+    });
+
+}
+
+function userLoggedIn() {
+    $("#linkLogin").hide();
+    $("#linkLogOut").show();
+    $("#linkMyAccount").show();
+    $("#linkMyPlans").show();
+}
+
+function userLoggOut() {
+    $("#linkLogin").show();
+    $("#linkLogOut").hide();
+    $("#linkMyAccount").hide();
+    $("#linkMyPlans").hide();
+}
+
+function User_InsertingUserInfo() {
+    var data = {
+        UserName: $("#txtRegisterLogin").val(),
+        Email: $("#txtRegisterEmail").val(),
+        Password: $("#txtRegisterPassword").val()
+    };
+    $.ajax({
+        type: "POST",
+        url: '/UserControls/User_InsertingUserInfo',
+        dataType: "json",
+        data: data,
+        beforeSend: function() {
+            $(".blockPage").show();
+        },
+        success: function(data) {
+            if (data.Success) {
+                IsUserLoggedIn();
+                $("#lblUserRegisterError").hide();
+            } else {
+                $("#lblUserRegisterError").show();
+            }
+        },
+        error: function(result) {
+            alert('Service call failed: ' + result.status + ' Type :' + result.statusText);
+        },
+        complete: function() {
+            $(".blockPage").hide();
+        }
+    });
+}
+
+function UserLogOut() {
+    $.ajax({
+        type: "POST",
+        url: '/UserControls/UserLogOut',
+        dataType: "json",
+        beforeSend: function () {
+        },
+        success: function (data) {
+            if (data.Success) {
+                IsUserLoggedIn();
+            } 
+        },
+        error: function (result) {
+            alert('Service call failed: ' + result.status + ' Type :' + result.statusText);
+        },
+        complete: function () {
+            $(".blockPage").hide();
+        }
+    });
 }
---
+++ PlanGoGo/Helper/BaseControler.cs
@@ -0,0 +1,53 @@
+﻿using BusinessEntites.Users;
+using Newtonsoft.Json;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PlanGoGo.Helper
+{
+    public class BaseControler: Controller
+    {
+        private string _cookieName = "planGoGo";
+
+        public static UserEntity userEntity { get; set; }
+        public void CreateCookie()
+        {
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                            _cookieName,
+                            DateTime.Now,
+                            DateTime.Now.AddDays(30),
+                            true,
+                            JsonConvert.SerializeObject(userEntity),
+                            FormsAuthentication.FormsCookiePath);
+            // Encrypt the ticket.
+            string encTicket = FormsAuthentication.Encrypt(ticket);
+            // Create the cookie.
+            Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encTicket));
+        }
+
+        public bool IsUserAuthenticated()
+        {
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                // Get the forms authentication ticket.
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket != null)
+                {
+                    userEntity = JsonConvert.DeserializeObject<UserEntity>(authTicket.UserData);
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+
+        public void LogOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
